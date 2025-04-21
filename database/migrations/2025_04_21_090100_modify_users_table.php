ca6e9173a43ce15d06649b7fd371eb712ac67e15
Oujaber Ousama <oujaberousama@gmail.com>
2025-04-21 09:00:00 +0100

    add migration

---
+++ database/migrations/2025_04_21_090100_modify_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('role_id')->after('id')->constrained('roles');
+            $table->string('first_name')->after('name')->nullable();
+            $table->string('last_name')->after('first_name')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamp('last_login_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id']);
+            $table->dropColumn(['role_id', 'first_name', 'last_name', 'is_active', 'last_login_at']);
+        });
+    }
+};
